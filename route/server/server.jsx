4228ddc4ee2bdfba14b9de1c79f6e0b5d46cae00
gusrns48 <gusrns48@naver.com>
2022-02-27 14:23:03 +0900

    fixed

--- route/server/server.jsx
+++ route/server/server.jsx
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
     host : "localhost",
     user : "root",
     password : "1234",
-    database : "tistory",
+    database : "ex",
 });
 */
 
@@ -22,15 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-/* 혼을 갈아넣은 API 형식입니다. DB에 맞춰서 사용하세요~
-/* API 수정하거나 추가되면 이 형식 맞추셔서 이용하시면 됩니당.
-app.get("/login", (req, res)=>){
+// 혼을 갈아넣은 API 형식입니다. DB에 맞춰서 사용하세요~
+// API 수정하거나 추가되면 이 형식 맞추셔서 이용하시면 됩니당.
+app.get("/login"), (req, res)=>{
     const user = "select * from usertable"
     connection.query(user, (err, result)=>{
         res.send(user);
     })
 }
-*/
 
 app.post("/register"), (req, res)=>{
     const name = req.body.name //이런식으로 정보 맞춰서 생성
