864dcd19daaa5ae45c275ca8d97e77c1fe3d5ca0
jiwonkim97 <jiwonkim97@naver.com>
2022-04-13 16:35:18 +0900

    feat: 슬픔 나누기 api, 로깅
    슬픔 나누기 api 추가 및
    로그 관리를 위한 morgan 세팅

--- routes/home.js
+++ routes/home.js
@@ -1,2 +1,18 @@
-//const mysqlConnection = require("../../modules/mysql");
-//const connection = mysqlConnection.connection;
+const mysqlConnection = require("../modules/mysql");
+const connection = mysqlConnection.connection;
+
+exports.getRandomProfiles = async (req, res) => {
+  const query = `SELECT *
+    FROM HOSTS
+    ORDER BY RAND()
+    LIMIT 3;`
+  connection.query(query, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({result : "DB 접근에 실패했습니다."})
+    }
+    else {
+      res.status(200).send({result : response})
+    }
+  })
+}
--- routes/login.js
+++
@@ -1,7 +0,0 @@
-//const mysqlConnection = require("../../modules/mysql");
-//const connection = mysqlConnection.connection;
-
-exports.test = async (req, res) => {
-  console.log('test called');
-  res.send({ result: 'test' });
-}
